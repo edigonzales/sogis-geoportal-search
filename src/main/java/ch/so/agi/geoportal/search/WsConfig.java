c9bfbbcba52d3805f6290aaa3f894cb19f47aca8
Stefan Ziegler <edi.gonzales@gmail.com>
2020-04-22 19:13:38 +0200

    change to domino ui

--- src/main/java/ch/so/agi/geoportal/search/WsConfig.java
+++ src/main/java/ch/so/agi/geoportal/search/WsConfig.java
@@ -4,25 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 public class WsConfig {
     @Bean
-    public HttpMessageConverter<Object> createXmlHttpMessageConverter(Jaxb2Marshaller marshaller) {
-        MarshallingHttpMessageConverter xmlConverter = new MarshallingHttpMessageConverter();
-        xmlConverter.setMarshaller(marshaller);
-        xmlConverter.setUnmarshaller(marshaller);
-        return xmlConverter;
-    }
-
-    @Bean
-    public Jaxb2Marshaller createMarshaller() {
-        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-        marshaller.setPackagesToScan("ch.ehi.oereb.schemas");
-        marshaller.setSupportJaxbElementClass(true);
-        marshaller.setLazyInit(true);
-        return marshaller;
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
     }
 }
 
--- src/main/java/ch/so/agi/geoportal/search/client/AppEntryPoint.java
+++ src/main/java/ch/so/agi/geoportal/search/client/AppEntryPoint.java
@@ -1,8 +1,29 @@
 package ch.so.agi.geoportal.search.client;
 
+import static elemental2.dom.DomGlobal.console;
+import static org.jboss.elemento.Elements.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
+
+import org.dominokit.domino.ui.dropdown.DropDownMenu;
+import org.dominokit.domino.ui.forms.SuggestBoxStore;
+import org.dominokit.domino.ui.forms.SuggestBox;
+import org.dominokit.domino.ui.forms.SuggestItem;
+import org.dominokit.domino.ui.forms.SuggestBox.DropDownPositionDown;
+import org.dominokit.domino.ui.forms.SuggestBoxStore.SuggestionsHandler;
+import org.dominokit.domino.ui.icons.Icons;
+import org.dominokit.domino.ui.style.Color;
+import org.dominokit.domino.ui.style.ColorScheme;
+import org.dominokit.domino.ui.tabs.Tab;
+import org.dominokit.domino.ui.tabs.TabsPanel;
+import org.dominokit.domino.ui.themes.Theme;
+import org.dominokit.domino.ui.utils.DelayedTextInput;
+import org.dominokit.domino.ui.utils.DelayedTextInput.DelayedAction;
+import org.dominokit.domino.ui.utils.HasChangeHandlers.ChangeHandler;
+import org.dominokit.domino.ui.utils.HasSelectionHandler.SelectionHandler;
+import org.jboss.elemento.EventType;
 
 //import org.dominokit.domino.ui.Typography.Paragraph;
 //import org.dominokit.domino.ui.cards.Card;
@@ -39,11 +60,11 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+import ch.qos.logback.classic.Logger;
 import ch.so.agi.geoportal.search.shared.SettingsResponse;
 import ch.so.agi.geoportal.search.shared.SettingsService;
 import ch.so.agi.geoportal.search.shared.SettingsServiceAsync;
@@ -54,6 +75,24 @@ import ch.so.agi.geoportal.search.shared.SettingsServiceAsync;
 //import elemental2.dom.HTMLDivElement;
 //import elemental2.dom.HTMLElement;
 //import elemental2.dom.Response;
+import elemental2.core.Global;
+import elemental2.core.JsArray;
+import elemental2.core.JsMap;
+import elemental2.core.JsNumber;
+import elemental2.core.JsObject;
+import elemental2.core.JsString;
+import elemental2.dom.DomGlobal;
+import elemental2.dom.Event;
+import elemental2.dom.EventListener;
+import elemental2.dom.HTMLDivElement;
+import elemental2.dom.HTMLElement;
+import elemental2.dom.Headers;
+import elemental2.dom.KeyboardEvent;
+import elemental2.dom.RequestInit;
+import jsinterop.base.Js;
+import jsinterop.base.JsPropertyMap;
+import ol.Coordinate;
+import ol.Extent;
 
 //import jsinterop.base.Js;
 //import jsinterop.base.JsPropertyMap;
@@ -63,10 +102,16 @@ public class AppEntryPoint implements EntryPoint {
     private final SettingsServiceAsync settingsService = GWT.create(SettingsService.class);
     
     private String MY_VAR;
-    
+    // &limit=XX
+    private String SEARCH_SERVICE_URL = "https://geo.so.ch/api/search/v2/?filter=foreground,ch.so.agi.gemeindegrenzen,ch.so.agi.av.gebaeudeadressen.gebaeudeeingaenge,ch.so.agi.av.bodenbedeckung,ch.so.agi.av.grundstuecke.projektierte,ch.so.agi.av.grundstuecke.rechtskraeftig,ch.so.agi.av.nomenklatur.flurnamen,ch.so.agi.av.nomenklatur.gelaendenamen&searchtext=";    
+//    private String SEARCH_SERVICE_URL = "https://api3.geo.admin.ch/rest/services/api/SearchServer?sr=2056&limit=15&type=locations&origins=address,parcel&searchText=";
+
     private NumberFormat fmtDefault = NumberFormat.getDecimalFormat();
     private NumberFormat fmtPercent = NumberFormat.getFormat("#0.0");
     
+    SuggestBox suggestBox;
+    HTMLDivElement resultPanel;
+
     public void onModuleLoad() {
         settingsService.settingsServer(new AsyncCallback<SettingsResponse>() {
             @Override
@@ -82,83 +127,222 @@ public class AppEntryPoint implements EntryPoint {
         });
     }
 
-    private void init() {        
-    	
-//        SuggestBoxStore dynamicStore =
-//                new SuggestBoxStore() {
-//
-//					@Override
-//					public void filter(String value, SuggestionsHandler suggestionsHandler) {
-//						GWT.log("filter");
-//						GWT.log(value);
-//		                DomGlobal.fetch("https://restcountries.eu/rest/v2/all")
-//                        .then(Response::text)
-//                        .then(json -> {
-//                            List<SuggestItem<String>> suggestItems = new ArrayList<>();
-//                            JsArray<JsPropertyMap<String>> randomNames = Js.cast(Global.JSON.parse(json));
-//                            for (int i = 0; i < randomNames.length; i++) {
-//                                JsPropertyMap<String> nameProperties = randomNames.getAt(i);
-//                                if (nameProperties.get("name").toLowerCase().contains(value.toLowerCase())) {
-//                                    SuggestItem suggestItem = SuggestItem.create(nameProperties.get("name"));
-//                                    suggestItems.add(suggestItem);
-//                                }
-//                            }
-//                            suggestionsHandler.onSuggestionsReady(suggestItems);
-//                            return null;
-//                        });
-//						
-//					}
-//
-//					@Override
-//					public void find(Object searchValue, Consumer handler) {
-//						GWT.log("find");
-//						GWT.log(searchValue.toString());
-//						GWT.log("handler: " + handler.toString());
-//		                DomGlobal.fetch("https://restcountries.eu/rest/v2/all")
-//                        .then(Response::text)
-//                        .then(json -> {
-//                            JsArray<JsPropertyMap<String>> randomNames = Js.cast(Global.JSON.parse(json));
-////                            for (int i = 0; i < randomNames.length; i++) {
-////                                JsPropertyMap<String> nameProperties = randomNames.getAt(i);
-////                                if (nameProperties.get("name").equals(searchValue)) {
-////                                    SuggestItem suggestItem = SuggestItem.create(nameProperties.get("name"));
-////                                    handler.accept(suggestItem);
-////                                    return null;
-////                                }
-////                            }
-//                            return null;
-//                        });
-//						
-//					}
-//				};
+    @SuppressWarnings("unchecked")
+    private void init() { 
+        Theme theme = new Theme(ColorScheme.BLUE);
+        theme.apply();
+
+        SuggestBoxStore dynamicStore = new SuggestBoxStore() {
+            @Override
+            public void filter(String value, SuggestionsHandler suggestionsHandler) {
+                if (value.trim().length() == 0) {
+                    return;
+                }
+                
+                RequestInit requestInit = RequestInit.create();
+                Headers headers = new Headers();
+                headers.append("Content-Type", "application/x-www-form-urlencoded"); // CORS and preflight...
+                requestInit.setHeaders(headers);
+                
+                // fetch(url, init) -> https://www.javadoc.io/doc/com.google.elemental2/elemental2-dom/1.0.0-RC1/elemental2/dom/RequestInit.html
+                // abort -> https://github.com/react4j/react4j-flux-challenge/blob/b9b28250fd3f954c690f874605f67e2a24a7274d/src/main/java/react4j/sithtracker/model/SithPlaceholder.java
+                DomGlobal.fetch(SEARCH_SERVICE_URL + value.trim().toLowerCase(), requestInit)
+                .then(response -> {
+                    if (!response.ok) {
+                        return null;
+                    }
+                    return response.text();
+                })
+                .then(json -> {
+                    List<SuggestItem<SearchResult>> suggestItems = new ArrayList<>();
+                    JsPropertyMap<?> parsed = Js.cast(Global.JSON.parse(json));
+                    JsArray<?> results = Js.cast(parsed.get("results"));
+                    for (int i = 0; i < results.length; i++) {
+                        JsPropertyMap<?> resultObj = Js.cast(results.getAt(i));
+                        if (resultObj.has("feature")) {
+                            JsPropertyMap feature = (JsPropertyMap) resultObj.get("feature");
+                            String display = ((JsString) feature.get("display")).normalize();
+                            
+                            SearchResult searchResult = new SearchResult();
+                            searchResult.setLabel(display);
+
+//                          // TODO icon type depending on address and parcel ?
+                            SuggestItem<SearchResult> suggestItem = SuggestItem.create(searchResult, searchResult.getLabel(), Icons.ALL.place());
+                            suggestItems.add(suggestItem);
+
+                        }
+
+//                        SearchResult searchResult = new SearchResult();
+//                        searchResult.setLabel("Fubar");
+//                        searchResult.setLabel(((JsString) attrs.get("label")).normalize());
+//                        searchResult.setOrigin(((JsString) attrs.get("origin")).normalize());
+//                        searchResult.setBbox(((JsString) attrs.get("geom_st_box2d")).normalize());
+//                        searchResult.setEasting(((JsNumber) attrs.get("y")).valueOf());
+//                        searchResult.setNorthing(((JsNumber) attrs.get("x")).valueOf());
+                        
+                    }
+                    suggestionsHandler.onSuggestionsReady(suggestItems);
+                    return null;
+                }).catch_(error -> {
+                    console.log(error);
+                    return null;
+                });
+            }
+
+            @Override
+            public void find(Object searchValue, Consumer handler) {
+                if (searchValue == null) {
+                    return;
+                }
+                SearchResult searchResult = (SearchResult) searchValue;
+                console.log(suggestBox.getInputElement().getTextContent());
+                SuggestItem<SearchResult> suggestItem = SuggestItem.create(searchResult, searchResult.getDisplay());
+                handler.accept(suggestItem);
+                console.log(suggestBox.getInputElement().element().innerHTML);
+            }
+        };
+
+        suggestBox = SuggestBox.create("Suche: Orte, Karten, Daten, ...", dynamicStore);
+        suggestBox.setIcon(Icons.ALL.search());
+        suggestBox.getInputElement().setAttribute("autocomplete", "off");
+        suggestBox.getInputElement().setAttribute("spellcheck", "false");
+        DropDownMenu suggestionsMenu = suggestBox.getSuggestionsMenu();
+        suggestionsMenu.setPosition(new DropDownPositionDown());
+        
+        
+        suggestionsMenu.addEventListener("keydown", evt -> {
+
+            KeyboardEvent keyboardEvent = Js.uncheckedCast(evt);
+            String key = keyboardEvent.key.toLowerCase();
+//            console.log("suggestionsMenu" + key);
+        });
+
+        // Funktioniert nicht wenn Suggestionspanel offen ist.
+        suggestBox.addEventListener("keydown", evt -> {
+
+            KeyboardEvent keyboardEvent = Js.uncheckedCast(evt);
+            String key = keyboardEvent.key.toLowerCase();
+//            console.log(key);
+        });
+        
+        suggestBox.setOnEnterAction(new DelayedAction() {
+
+            @Override
+            public void doAction() {
+
+//                console.log("enter pressed.");
+            }
+            
+        });
+        
+        suggestBox.addChangeHandler(new ChangeHandler() {
+            @Override
+            public void onValueChanged(Object value) {
+//                console.log("onValueChange");
+                
+                
+            }
+            
+        });
+        
+        suggestBox.addSelectionHandler(new SelectionHandler() {
+            @Override
+            public void onSelection(Object value) {
+                console.log("onSelection");
+                
+                
+                console.log(suggestionsMenu.getTextContent());
+                
+//                loader.stop();
+//                resetGui();
+
+                SuggestItem<SearchResult> item = (SuggestItem<SearchResult>) value;
+                SearchResult result = (SearchResult) item.getValue();
+                console.log("****" + result.getDisplay());
+                
+//                String[] coords = result.getBbox().substring(4,result.getBbox().length()-1).split(",");
+//                String[] coordLL = coords[0].split(" ");
+//                String[] coordUR = coords[1].split(" ");
+//                Extent extent = new Extent(Double.valueOf(coordLL[0]).doubleValue(), Double.valueOf(coordLL[1]).doubleValue(), 
+//                Double.valueOf(coordUR[0]).doubleValue(), Double.valueOf(coordUR[1]).doubleValue());
+//                
+//                double easting = Double.valueOf(result.getEasting()).doubleValue();
+//                double northing = Double.valueOf(result.getNorthing()).doubleValue();
+//                
+//                Coordinate coordinate = new Coordinate(easting, northing);
+//                sendCoordinateToServer(coordinate.toStringXY(3), null);
+                
+                // TODO: remove focus
+                // -> Tried a lot but failed. Ask the authors.
+                
+                
+                
+                resultPanel.style.visibility = "visible";
+            }
+        });
+	
+        HTMLElement suggestBoxDiv = div().id("suggestBoxDiv").add(suggestBox).element();
+        body().add(div().id("searchPanel").add(div().id("suggestBoxDiv").add(suggestBox)));
+
+        Tab tabAll = Tab.create("Alle".toUpperCase())
+                .style()
+                .setWidth("25%")
+                .get();
+        Tab tabPlaces = Tab.create("Orte".toUpperCase())
+                .style()
+                .setWidth("25%")
+                .get();
+        Tab tabMap = Tab.create("Karten".toUpperCase())
+                .style()
+                .setWidth("25%")
+                .get();
+        Tab tabData = Tab.create("Daten".toUpperCase())
+                .style()
+                .setWidth("25%")
+                .get();
+
+        HTMLElement tabsPanel = TabsPanel.create().setId("resultTabs")
+            .setBackgroundColor(Color.WHITE)
+            .setColor(Color.BLUE)
+            .appendChild(tabAll)
+            .appendChild(tabPlaces)
+            .appendChild(tabMap.appendChild(span().textContent("N/A")))
+            .appendChild(tabData)
+            .element();
+
+        resultPanel = div().id("resultPanel").add(tabsPanel).element();
+        resultPanel.style.visibility = "hidden";
+        
+        body().add(resultPanel);
 
         
         
-	
+        
+        
 //        TabsPanel tabsPanel = TabsPanel.create();
 
         
-        FlowPanel searchBoxContainer = new FlowPanel();
-        searchBoxContainer.setStyleName("searchBoxContainer");
-
-        HorizontalPanel searchPanel = new HorizontalPanel();
-        searchPanel.setStyleName("searchPanel");
-        searchPanel.setSpacing(5);
-
-        MultiWordSuggestOracle oracle = new MultiWordSuggestOracle();  
-        oracle.add("Cat");
-        oracle.add("Dog");
-        oracle.add("Horse");
-        oracle.add("Canary");
-        
-        SuggestBox suggestBox = new SuggestBox(oracle, new TextBox(), new CustomSuggestionDisplay());
-        suggestBox.setWidth("100%");
-        suggestBox.setAutoSelectEnabled(false);
-        
-        suggestBox.addValueChangeHandler(h -> {
-            GWT.log("addValueChangeHandler: " + h.getValue()); 
-        });
-        
+//        FlowPanel searchBoxContainer = new FlowPanel();
+//        searchBoxContainer.setStyleName("searchBoxContainer");
+//
+//        HorizontalPanel searchPanel = new HorizontalPanel();
+//        searchPanel.setStyleName("searchPanel");
+//        searchPanel.setSpacing(5);
+//
+//        MultiWordSuggestOracle oracle = new MultiWordSuggestOracle();  
+//        oracle.add("Cat");
+//        oracle.add("Dog");
+//        oracle.add("Horse");
+//        oracle.add("Canary");
+//        
+//        SuggestBox suggestBox = new SuggestBox(oracle, new TextBox(), new CustomSuggestionDisplay());
+//        suggestBox.setWidth("100%");
+//        suggestBox.setAutoSelectEnabled(false);
+//        
+//        suggestBox.addValueChangeHandler(h -> {
+//            GWT.log("addValueChangeHandler: " + h.getValue()); 
+//        });
+//        
 //        suggestBox.addSelectionHandler(h -> {
 //            GWT.log("addSelectionHandler: " + h.getSelectedItem().getReplacementString()); 
 //            
@@ -176,31 +360,31 @@ public class AppEntryPoint implements EntryPoint {
 //            //tabsPanel.style().setMaxWidth("100%", true);
 //            
 //        });
-        
-        suggestBox.addKeyDownHandler(h -> {
-           //GWT.log(((SuggestBox) h.getSource()).getValue()); 
-            if (h.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-                GWT.log("ENTER");
-                
-                
-                GWT.log(suggestBox.getText());
-                
-                
-            }
-        });
-        searchPanel.add(suggestBox);
-        searchBoxContainer.add(searchPanel);
-        
-        Button searchButton = new Button("Suche", new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				GWT.log("Button clicked.");
-			}
-        });
-        searchPanel.add(searchButton);
-        
-        
-        RootPanel.get().add(searchBoxContainer);
+//        
+//        suggestBox.addKeyDownHandler(h -> {
+//           //GWT.log(((SuggestBox) h.getSource()).getValue()); 
+//            if (h.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+//                GWT.log("ENTER");
+//                
+//                
+//                GWT.log(suggestBox.getText());
+//                
+//                
+//            }
+//        });
+//        searchPanel.add(suggestBox);
+//        searchBoxContainer.add(searchPanel);
+//        
+//        Button searchButton = new Button("Suche", new ClickHandler() {
+//			@Override
+//			public void onClick(ClickEvent event) {
+//				GWT.log("Button clicked.");
+//			}
+//        });
+//        searchPanel.add(searchButton);
+//        
+//        
+//        RootPanel.get().add(searchBoxContainer);
 
         
         // Domino UI
